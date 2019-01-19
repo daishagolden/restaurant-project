79a9f85042dcc705c1235b126955ae41671c88df
unknown <daishagolden430@gmail.com>
2019-01-18 19:29:03 -0600

    first commit

---
+++ js/restaurant.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slide1').slick({
+      infinite: true,
+      dots: false,
+      slidesToShow: 1,
+      slidesToScroll: 1, 
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });   
+});
